62113b7622e2c1c50973b635e1663bc96dcf40a9
Anton Ukhanev <xedin.unknown@gmail.com>
2018-09-15 16:14:40 +0200

    Added services definition file

---
+++ services.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Contains service definitions used by the plugin.
+ *
+ * @package PcfWooCommerce
+ */
+
+use XedinUnknown\PcfWooCommerce\DI_Container;
+
+/**
+ * A factory of a service definition map.
+ *
+ * @since 0.1
+ *
+ * @param string $base_path Path to the plugin file.
+ * @param string $base_url URL of the plugin folder.
+ *
+ * @return array A map of service names to service definitions.
+ */
+return function ( $base_path, $base_url ) {
+		return [
+			'version'                 => '0.1',
+			'base_path'               => $base_path,
+			'base_dir'                => dirname( $base_path ),
+			'base_url'                => $base_url,
+			'js_path'                 => '/assets/js',
+			'product_code_field_name' => '_ean_field',
+		];
+};
