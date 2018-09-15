e8508cb3d3ed216b5c947fbbe7cb3a190530f46f
Anton Ukhanev <xedin.unknown@gmail.com>
2018-09-15 18:05:26 +0200

    Added a link template

--- templates/link.php
+++ templates/link.php
@@ -1,1 +1,17 @@
-<a href="<?php echo $c('href') ?>" class="thickbox" title="<?php echo $c('title') ?>"><?php echo $c('content') ?></a>
+<?php
+/**
+ * A template for an HTML anchor.
+ *
+ * @package PcfWooCommerce
+ *
+ * @var $c callable The function used to retrieve context values.
+ */
+
+?>
+<a
+	href="<?php echo esc_attr( $c( 'href' ) ); ?>"
+	class="<?php echo esc_attr( $c( 'class' ) ); ?>"
+	title="<?php echo esc_attr( $c( 'title' ) ); ?>"
+>
+	<?php echo esc_html( $c( 'content' ) ); ?>
+</a>
